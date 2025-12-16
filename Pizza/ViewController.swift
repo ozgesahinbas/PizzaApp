3091b0211e50fa2620e3a89852ec800f6326470c
ozgesahinbas <ozge.sahinbas58@gmail.com>
2025-12-16 13:33:14 +0300

    Feat estimatedTime method

--- Pizza/ViewController.swift
+++ Pizza/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let margarita = Pizza(name: "Margarita", imageName: "dkkdk", basePrice: 35.2)
+        let margaritaPriceSmall = calculatePrice(pizza: margarita, size: .small)
+        let margaritaPriceMedium = calculatePrice(pizza: margarita, size: .medium)
+        let margaritaPriceLarge = calculatePrice(pizza: margarita, size: .large)
         
     }
+    
+    
     func calculatePrice(pizza: Pizza, size: PizzaSize) -> Float {
         switch size {
         case .small:
@@ -22,6 +28,17 @@ class ViewController: UIViewController {
             return pizza.basePrice * 1.5
         case .large:
             return pizza.basePrice * 2.0
+        }
+    }
+    
+    func estimatedTime(pizza: Pizza, size: PizzaSize) -> String {
+        switch size {
+        case .small:
+            return "10-20 min."
+        case .medium:
+            return "20-30 min."
+        case .large:
+            return "30-40 min."
         }
     }
 }
