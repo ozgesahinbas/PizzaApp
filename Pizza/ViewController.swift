f3b514fb9aaada2cdd72438b9ef2fe2b8de5c3ed
ozgesahinbas <ozge.sahinbas58@gmail.com>
2025-12-16 13:31:10 +0300

    Create PizzaApp project

---
+++ Pizza/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Pizza
+//
+//  Created by Özge Şahinbaş on 12.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
